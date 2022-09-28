37e3e3d8d7fb754671ebcf6f861c9df1909a1e6a
Peder Aalen <peaa@computas.com>
2022-09-28 19:00:27 +0200

    Legg til swagger

--- BuberBreakfast/Program.cs
+++ BuberBreakfast/Program.cs
@@ -3,13 +3,24 @@ using BuberBreakfast.Services.Breakfasts;
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
     builder.Services.AddScoped<IBreakfastService, BreakfastService>();
 }
 
+
 var app = builder.Build();
 {
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
     app.UseExceptionHandler("/error");
-    app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
 }
